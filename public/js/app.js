cd2f6b7e08157752ea4b1316109ba79a7cb7f883
dream-well <80394710+georgesterm123@users.noreply.github.com>
2021-07-13 22:51:51 -0700

    update jul13

--- public/js/app.js
+++ public/js/app.js
@@ -86991,6 +86991,9 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
+//
 
 
 
@@ -88249,59 +88252,45 @@ var render = function() {
                             "col-12 col-md-8 offset-md-2 search-page__nav_search"
                         },
                         [
-                          _c("div", { staticClass: "search-icon" }, [
-                            _c(
-                              "svg",
-                              {
-                                staticClass: "sm-search-svg",
-                                staticStyle: {
-                                  "enable-background": "new 0 0 56.966 56.966"
-                                },
-                                attrs: {
-                                  xmlns: "http://www.w3.org/2000/svg",
-                                  "xmlns:xlink": "http://www.w3.org/1999/xlink",
-                                  version: "1.1",
-                                  x: "0px",
-                                  y: "0px",
-                                  viewBox: "0 0 56.966 56.966",
-                                  "xml:space": "preserve",
-                                  width: "100%"
-                                }
-                              },
-                              [
-                                _c("path", {
+                          _c("div", { staticClass: "search-div" }, [
+                            _c("div", { staticClass: "search-icon" }, [
+                              _c(
+                                "svg",
+                                {
+                                  staticClass: "sm-search-svg",
+                                  staticStyle: {
+                                    "enable-background": "new 0 0 56.966 56.966"
+                                  },
                                   attrs: {
-                                    d:
-                                      "M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z",
-                                    fill: "#ffffff"
+                                    xmlns: "http://www.w3.org/2000/svg",
+                                    "xmlns:xlink":
+                                      "http://www.w3.org/1999/xlink",
+                                    version: "1.1",
+                                    x: "0px",
+                                    y: "0px",
+                                    viewBox: "0 0 56.966 56.966",
+                                    "xml:space": "preserve",
+                                    width: "100%"
                                   }
-                                })
-                              ]
-                            )
+                                },
+                                [
+                                  _c("path", {
+                                    attrs: {
+                                      d:
+                                        "M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z",
+                                      fill: "#ffffff"
+                                    }
+                                  })
+                                ]
+                              )
+                            ])
                           ]),
                           _vm._v(" "),
                           _c("input", {
-                            directives: [
-                              {
-                                name: "model",
-                                rawName: "v-model",
-                                value: _vm.search_query,
-                                expression: "search_query"
-                              }
-                            ],
                             staticClass: "form-control mr-sm-2",
                             attrs: {
                               type: "text",
                               placeholder: _vm.$t("home.search")
-                            },
-                            domProps: { value: _vm.search_query },
-                            on: {
-                              input: function($event) {
-                                if ($event.target.composing) {
-                                  return
-                                }
-                                _vm.search_query = $event.target.value
-                              }
                             }
                           })
                         ]
